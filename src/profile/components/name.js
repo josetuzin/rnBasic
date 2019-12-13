655e745d981849d9a78fbd4296baae2a9886f798
José Tuzinkievicz <josetuzin@gmail.com>
2019-12-13 08:36:15 -0300

    Pasando navigation props a cualquier componente

---
+++ src/profile/components/name.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    View,
+    Text
+} from 'react-native'
+
+import { withNavigation } from 'react-navigation'
+
+function Name(props) {
+    return(
+        <View>
+            <Text>{props.navigation.getParam('name')}</Text>
+        </View>
+    )
+}
+
+export default withNavigation(Name)
